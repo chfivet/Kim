8847e376b2fa40ab5f83a1bbfdee0251012f992f
Christophe Fivet <christophe.fivet@uclouvain.be>
2020-06-16 19:39:10 +0200

    issue#11 - Add __str__ for Person - Add username for Person

--- kim/person/models/person.py
+++ kim/person/models/person.py
@@ -12,3 +12,14 @@ class Person (models.Model):
     phone = models.TextField(max_length=30, null=True)
     phone_mob = models.TextField(max_length=30, null=True)
     user = models.OneToOneField(User, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        if self.middle_name:
+            return "{} {} {}".format(self.first_name, self.middle_name, self.last_name)
+        else:
+            return "{} {}".format(self.first_name, self.last_name)
+
+    def username(self):
+        if self.user is None:
+            return None
+        return self.user.username
