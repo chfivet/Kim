6630409af9efb9397df22f14a915c51450f689c2
daquinp <paul.daquin@hotmail.com>
2020-06-16 13:49:36 +0200

    patron person à jour

--- kim/backoffice/settings.py
+++ kim/backoffice/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'person.apps.PersonConfig',
+    'person',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- kim/person/models/person.py
+++ kim/person/models/person.py
@@ -5,9 +5,8 @@ from django.forms import DateField
 class Person (models.Model):
     date_last_change = models.DateField(auto_now=True)
     first_name = models.CharField(max_length=50)
-"""
     last_name = models.CharField(max_length=50)
-    gender = models.CharField(max_length=1, choice= [
+    gender = models.CharField(max_length=5, choices= [
         ('HOMME', 'Homme'),
         ('FEMME', 'Femme'),
     ])
@@ -15,4 +14,3 @@ class Person (models.Model):
     phone = models.TextField(max_length=10)
     phone_mob = models.TextField(max_length=10)
     birth_date = models.DateTimeField(auto_now=False, auto_now_add=False)
-"""
